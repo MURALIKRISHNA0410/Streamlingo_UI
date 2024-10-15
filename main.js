6430081e23dff44f833c09fa37a239c169e89a42
Murali Krishna <140298932+MURALIKRISHNA0410@users.noreply.github.com>
2024-10-15 18:46:07 +0530

    added turn servers

--- main.js
+++ main.js
@@ -38,7 +38,29 @@ const PeerConnection = (function () {
         {
           urls: ["stun:stun1.l.google.com:19302",
         "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302"]
+        "stun:stun3.l.google.com:19302"],{
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80",
+        username: "317a6a96726c601c4a4cd2ed",
+        credential: "9pAW0VLDBGOgYxxq",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username: "317a6a96726c601c4a4cd2ed",
+        credential: "9pAW0VLDBGOgYxxq",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:443",
+        username: "317a6a96726c601c4a4cd2ed",
+        credential: "9pAW0VLDBGOgYxxq",
+      },
+      {
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username: "317a6a96726c601c4a4cd2ed",
+        credential: "9pAW0VLDBGOgYxxq",
+      },
 
         },
       ],
